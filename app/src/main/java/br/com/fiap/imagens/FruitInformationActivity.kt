b53e9a7f06b9788a1205f668c8dae11300058841
Luiz Guilherme Gaioto Teixeira <Luiz.guilherme.gaioto@outlook.com>
2020-03-02 21:59:00 -0300

    Cor de fundo

--- app/src/main/java/br/com/fiap/imagens/FruitInformationActivity.kt
+++ app/src/main/java/br/com/fiap/imagens/FruitInformationActivity.kt
@@ -1,14 +1,58 @@
 package br.com.fiap.imagens
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_fruit_information.*
 
-class FruitInformation : AppCompatActivity(){
+class FruitInformationActivity : AppCompatActivity(){
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        getSupportActionBar()?.hide();
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_fruit_information)
+        setImage()
     }
+
+    private fun setImage() {
+        when(this.intent.getIntExtra("SELECIONADO", -1)){
+            0 -> {
+               image_fruit.setImageResource(R.drawable.abacaxi)
+               fruit_name.text = getString(R.string.txt_abacaxi)
+               fruit_info.text = getString(R.string.abacaxi_info)
+               tela_fruta.background = getDrawable(R.color.color_abacaxi)
+            }
+
+            1 -> {
+                image_fruit.setImageResource(R.drawable.banana)
+                fruit_name.text = getString(R.string.txt_banana)
+                fruit_info.text = getString(R.string.banana_info)
+                tela_fruta.background = getDrawable(R.color.color_banana)
+            }
+
+            2 -> {
+                image_fruit.setImageResource(R.drawable.manga)
+                fruit_name.text = getString(R.string.txt_manga)
+                fruit_info.text = getString(R.string.manga_info)
+                tela_fruta.background = getDrawable(R.color.color_manga)
+            }
+
+            3 -> {
+                image_fruit.setImageResource(R.drawable.morango)
+                fruit_name.text = getString(R.string.txt_morango)
+                fruit_info.text = getString(R.string.morango_info)
+                tela_fruta.background = getDrawable(R.color.color_morango)
+            }
+
+            4 -> {
+                image_fruit.setImageResource(R.drawable.melancia)
+                fruit_name.text = getString(R.string.txt_melancia)
+                fruit_info.text = getString(R.string.melancia_info)
+                tela_fruta.background = getDrawable(R.color.color_melancia)
+            }
+        }
+
+    }
+
 }
+
+
--- app/src/main/java/br/com/fiap/imagens/MainActivity.kt
+++ app/src/main/java/br/com/fiap/imagens/MainActivity.kt
@@ -1,12 +1,16 @@
 package br.com.fiap.imagens
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.Global.putInt
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var selecionado : Int = 0
 
     val imagens = intArrayOf(
         R.drawable.abacaxi,
@@ -16,14 +20,28 @@ class MainActivity : AppCompatActivity() {
         R.drawable.melancia
     )
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getSupportActionBar()?.hide();
+        getSupportActionBar()?.hide()
         setContentView(R.layout.activity_main)
+        listenClicks()
     }
 
     fun trocarImagem(view : View) {
         val sorteado = Random() .nextInt (imagens.size)
+        selecionado = sorteado
         imgFrutas.setImageResource( imagens[sorteado])
+    }
+
+    fun listenClicks() {
+        imgFrutas.setOnClickListener {
+
+            var intent = Intent(this, FruitInformationActivity::class.java).apply {
+                putExtra("SELECIONADO", selecionado)
+            }
+
+            startActivity(intent)
+        }
     }
 }
